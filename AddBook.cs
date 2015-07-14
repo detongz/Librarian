ad1b52b763a523533b24a3982cb61f818b7a4255
tbbrave <detong.chang@foxmail.com>
2015-07-14 18:38:12 +0800

    first commit

---
+++ AddBook.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace LibraryManagementSystem
+{
+    public partial class AddBook : Form
+    {
+        private string coverFilePath;//保存封面图片的路径
+        public AddBook()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQ_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// 保存图书信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string pat3 = @"^[1-9]\d*\.\d*|0\.\d*[1-9]\d*$";//浮点数
+            bool m3 = Regex.IsMatch(txtPrice.Text, pat3);
+            string pat2 = @"\d{4}-\d{2}-\d{2}";
+            bool m2 = Regex.IsMatch(txtPressDate.Text, pat2);
+
+
+            if (txtISBN.Text == "") MessageBox.Show("ISBN号不能为空！", "提示");
+            else if (txtName.Text == "") MessageBox.Show("书名不能为空！", "提示");
+            else if (txtAuthor.Text == "") MessageBox.Show("作者不能为空！", "提示");
+            else if (txtStock.Text == "") MessageBox.Show("作者不能为空！", "提示");
+            else if (!m3) MessageBox.Show("图书价格应为XX.XX元！", "提示！");
+            else if (!m2) MessageBox.Show("时间格式错误！", "提示！");
+            else
+            {
+                //连接数据库
+                
+                //按ISBN号和书名查询是否已经有记录存在
+
+                if (1!=2)//判断是否添加了相同的记录 
+                {
+                    MessageBox.Show("对不起!不允许填写相同记录(已有相同ISBN号书籍信息)！");
+                }
+                if (1==1)
+                {
+                    try
+                    {
+                        //向数据库中插入数据
+                        //获取的图片路径保存在coverFilePath
+                        MessageBox.Show("添加成功!");
+
+                    }
+                    finally
+                    {
+                        //关闭数据库
+                    }
+                }
+            } 
+        }
+        /// <summary>
+        /// 添加图书封面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            ofdViewPic.InitialDirectory = Application.StartupPath;//设置打开时的默认目录
+            ofdViewPic.Filter = "图片文件(*.jpg,*.gif,*.bmp)|*.jpg;*.gif;*.bmp";//控件打开的文件类型
+            ofdViewPic.RestoreDirectory = true;
+            ofdViewPic.CheckFileExists = true;
+            ofdViewPic.CheckPathExists = true;
+            ofdViewPic.Multiselect = false;
+            if (ofdViewPic.ShowDialog() == DialogResult.OK)
+            {
+                //向PictureBox中装入图片
+                coverFilePath = ofdViewPic.FileName;
+                pbCover.ImageLocation = ofdViewPic.FileName;
+            }
+            else
+            {
+                MessageBox.Show("你没有选择图片!", "信息提示");
+            }
+        }
+        /// <summary>
+        /// 控制库存只能为数字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtStock_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;//判断为数字
+            }
+        }
+    }
+}
---
+++ Reader.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class Reader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSearch = new System.Windows.Forms.ComboBox();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPressDate = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pbCover = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnALL = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "查询方式";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "关键字";
+            // 
+            // cmbSearch
+            // 
+            this.cmbSearch.FormattingEnabled = true;
+            this.cmbSearch.Items.AddRange(new object[] {
+            "书名",
+            "作者",
+            "出版社"});
+            this.cmbSearch.Location = new System.Drawing.Point(88, 25);
+            this.cmbSearch.Name = "cmbSearch";
+            this.cmbSearch.Size = new System.Drawing.Size(121, 20);
+            this.cmbSearch.TabIndex = 2;
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(88, 60);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(100, 21);
+            this.txtKeyWord.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(195, 57);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "搜索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnBorrow
+            // 
+            this.btnBorrow.Location = new System.Drawing.Point(236, 25);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrow.TabIndex = 5;
+            this.btnBorrow.Text = "借书";
+            this.btnBorrow.UseVisualStyleBackColor = true;
+            this.btnBorrow.Click += new System.EventHandler(this.btnBorrow_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(317, 25);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.TabIndex = 6;
+            this.btnReturn.Text = "还书";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(358, 57);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "刷新";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Location = new System.Drawing.Point(12, 136);
+            this.dgvBook.MultiSelect = false;
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.ReadOnly = true;
+            this.dgvBook.RowTemplate.Height = 23;
+            this.dgvBook.Size = new System.Drawing.Size(421, 301);
+            this.dgvBook.TabIndex = 8;
+            this.dgvBook.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(648, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "详细信息";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ISBN号";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(538, 53);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.ReadOnly = true;
+            this.txtISBN.Size = new System.Drawing.Size(174, 21);
+            this.txtISBN.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(471, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "书名";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(538, 92);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(174, 21);
+            this.txtName.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(471, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "作者";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(538, 138);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.ReadOnly = true;
+            this.txtAuthor.Size = new System.Drawing.Size(174, 21);
+            this.txtAuthor.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(471, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "出版社";
+            // 
+            // txtPress
+            // 
+            this.txtPress.Location = new System.Drawing.Point(538, 178);
+            this.txtPress.Name = "txtPress";
+            this.txtPress.ReadOnly = true;
+            this.txtPress.Size = new System.Drawing.Size(174, 21);
+            this.txtPress.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(471, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "出版日期";
+            // 
+            // txtPressDate
+            // 
+            this.txtPressDate.Location = new System.Drawing.Point(538, 217);
+            this.txtPressDate.Name = "txtPressDate";
+            this.txtPressDate.ReadOnly = true;
+            this.txtPressDate.Size = new System.Drawing.Size(174, 21);
+            this.txtPressDate.TabIndex = 19;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(538, 255);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(174, 21);
+            this.txtPrice.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(471, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "单价";
+            // 
+            // pbCover
+            // 
+            this.pbCover.Location = new System.Drawing.Point(739, 56);
+            this.pbCover.Name = "pbCover";
+            this.pbCover.Size = new System.Drawing.Size(151, 220);
+            this.pbCover.TabIndex = 22;
+            this.pbCover.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(471, 298);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 12);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "内容简介";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(538, 301);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(326, 113);
+            this.txtContent.TabIndex = 24;
+            // 
+            // btnALL
+            // 
+            this.btnALL.Location = new System.Drawing.Point(276, 57);
+            this.btnALL.Name = "btnALL";
+            this.btnALL.Size = new System.Drawing.Size(75, 23);
+            this.btnALL.TabIndex = 43;
+            this.btnALL.Text = "全部";
+            this.btnALL.UseVisualStyleBackColor = true;
+            this.btnALL.Click += new System.EventHandler(this.btnALL_Click);
+            // 
+            // Reader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 449);
+            this.Controls.Add(this.btnALL);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pbCover);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtPressDate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPress);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvBook);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnBorrow);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyWord);
+            this.Controls.Add(this.cmbSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Reader";
+            this.Text = "图书信息";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Reader_FormClosing);
+            this.Load += new System.EventHandler(this.Reader_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbSearch;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnBorrow;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPressDate;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pbCover;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnALL;
+    }
+}
